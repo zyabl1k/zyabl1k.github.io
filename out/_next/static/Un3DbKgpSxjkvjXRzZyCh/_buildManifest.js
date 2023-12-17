b5c4652c90f027e2bb3b23f162d27660394d05d7
zyabl1k <kdaniild@inbox.ru>
2023-12-18 01:46:46 +0500

    test

--- out/_next/static/Un3DbKgpSxjkvjXRzZyCh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/0b0ee452c776995c.css","static/chunks/pages/index-49d3c5c7cebcee2d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
